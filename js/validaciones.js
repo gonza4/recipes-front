031a65017b9e4f7478b8124363c83eaf5e6bfbf1
Alvaro Gomez <32045151+alvaroAGomez@users.noreply.github.com>
2019-05-15 22:23:23 -0300

    Correcion del Boton mostrar mas

--- js/validaciones.js
+++ js/validaciones.js
@@ -9,7 +9,7 @@ function validar(frm) {
 		}
 
 		toastr["error"]("Minimo 3 caracteres", "Error")
-	
+		
 		frm.txtBuscar.focus(); 
 
 		return false;
@@ -24,7 +24,7 @@ function validar(frm) {
 		}
 
 		toastr["error"]("No se permiten valores numericos en la busqueda", "Error")
-	
+		
 		frm.txtBuscar.focus(); 
 
 		return false;
@@ -33,35 +33,25 @@ function validar(frm) {
 	return true;
 } 
 function validarSoloTexto(parametro){
-		var patron=/^[a-zA-Z\s]*$/;
-		if (parametro.search(patron)) {
-			return false;
-		}else{
-			return true;
-		}
+	var patron=/^[a-zA-Z\s]*$/;
+	if (parametro.search(patron)) {
+		return false;
+	}else{
+		return true;
+	}
 }
 
 function mostrarOtrasCategorias(id){
 
-if(document.getElementById(id).style.display == 'block'){
-	document.getElementById(id).style.display ='none';
-	document.getElementById('b'+id).value='Ver Mas';
-	
-}else{
-	document.getElementById(id).style.display = 'block';
-	document.getElementById('b'+id).value= 'Ocultar';
+	if(document.getElementById(id).style.display == 'block'){
+		document.getElementById(id).style.display ='none';
+		if (document.getElementById('bmb').innerText="VER MENOS") {
+			document.getElementById('bmb').innerText="VER MAS"
+		}
+	}else{
+		document.getElementById(id).style.display = 'block';
+		if (document.getElementById('bmb').innerText="VER MAS") {
+			document.getElementById('bmb').innerText="VER MENOS"
+		}
+	}
 }
-
-}
-
-function muestra_oculta(id){
-if (document.getElementById){ //se obtiene el id
-var el = document.getElementById(id); //se define la variable "el" igual a nuestro div
-el.style.display = (el.style.display == 'none') ? 'block' : 'none'; //damos un atributo display:none que oculta el div
-}
-}
-window.onload = function(){/*hace que se cargue la función lo que predetermina que div estará oculto hasta llamar a la función nuevamente*/
-muestra_oculta('contenido_a_mostrar');/* "contenido_a_mostrar" es el nombre de la etiqueta DIV que deseamos mostrar */
-}
-
-
