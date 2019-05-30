8face1ab2983878217476bea6c9171e44fb9cb51
Alvaro Gomez <32045151+alvaroAGomez@users.noreply.github.com>
2019-05-30 01:13:04 -0300

    Nueva receta v2
    2da version de la pantalla nueva receta, incluye validaciones basicas,la funcionalidad basica esta sin la vinculacion al back, falta msj de confirmacion y otras validaciones

--- js/validaciones.js
+++ js/validaciones.js
@@ -1,28 +1,8 @@
 
-
-function vistaPrevia(){
-document.getElementById("file").onchange = function(e) {
-	
-	
-	let reader = new FileReader();
-  
-  reader.onload = function(){
-    let preview = document.getElementById('preview'),
-    		image = document.createElement('img');
-
-    image.src = reader.result;
-    
-    preview.innerHTML = '';
-    preview.append(image);
-  };
- 
-  reader.readAsDataURL(e.target.files[0]);
-}
-}
-
 function limpiarCampos(){
-	document.getElementById("file").value="";
+	document.getElementById("archivo").value="";
 	document.getElementById("preview").innerHTML="";
+	//falta agregar q las categorias seleccionadas dejen de estarlo
 }
 
 function validar(frm) { 
@@ -84,3 +64,115 @@ function mostrarOtrasCategorias(id){
 	}
 }
 */
+/*CODIGO PARA LA VISTA PREVIA DE LA IMAGEN*/
+window.imagenVacia = 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==';
+
+window.mostrarVistaPrevia = function mostrarVistaPrevia() {
+
+    var Archivos, Lector;
+
+    //Para navegadores antiguos
+    if (typeof FileReader !== "function") {
+        jQuery('#infoNombre').text('[Vista previa no disponible]');
+        jQuery('#infoTamaño').text('(su navegador no soporta vista previa!)');
+        return;
+    }
+
+    Archivos = jQuery('#archivo')[0].files;
+    if (Archivos.length > 0) {
+
+        Lector = new FileReader();
+        Lector.onloadend = function(e) {
+            var origen, tipo;
+
+            //Envia la imagen a la pantalla
+            origen = e.target; //objeto FileReader
+            //Prepara la información sobre la imagen
+            tipo = window.obtenerTipoMIME(origen.result.substring(0, 30));
+
+        //    jQuery('#infoNombre').text(Archivos[0].name + ' (Tipo: ' + tipo + ')');
+          //  jQuery('#infoTamaño').text('Tamaño: ' + e.total + ' bytes');
+            //Si el tipo de archivo es válido lo muestra, 
+            //sino muestra un mensaje 
+            if (tipo !== 'image/jpeg' && tipo !== 'image/png' && tipo !== 'image/gif') {
+                jQuery('#vistaPrevia').attr('src', window.imagenVacia);
+                alert('El formato de imagen no es válido: debe seleccionar una imagen JPG, PNG o GIF.');
+            } else {
+                jQuery('#vistaPrevia').attr('src', origen.result);
+            //    window.obtenerMedidas();
+            }
+
+        };
+        Lector.onerror = function(e) {
+            console.log(e)
+        }
+        Lector.readAsDataURL(Archivos[0]);
+
+    } else {
+        var objeto = jQuery('#archivo');
+        objeto.replaceWith(objeto.val('').clone());
+        jQuery('#vistaPrevia').attr('src', window.imagenVacia);
+        jQuery('#infoNombre').text('[Seleccione una imagen]');
+        jQuery('#infoTamaño').text('');
+    };
+
+
+};
+
+//Lee el tipo MIME de la cabecera de la imagen
+window.obtenerTipoMIME = function obtenerTipoMIME(cabecera) {
+    return cabecera.replace(/data:([^;]+).*/, '\$1');
+}
+
+//Obtiene las medidas de la imagen y las agrega a la 
+//etiqueta infoTamaño
+//window.obtenerMedidas = function obtenerMedidas() {
+//    jQuery('<img/>').bind('load', function(e) {
+//
+//        var tamaño = jQuery('#infoTamaño').text() + '; Medidas: ' + this.width + 'x' + this.height;
+//
+  //      jQuery('#infoTamaño').text(tamaño);
+
+    //}).attr('src', jQuery('#vistaPrevia').attr('src'));
+//}
+
+jQuery(document).ready(function() {
+
+    //Cargamos la imagen "vacía" que actuará como Placeholder
+    jQuery('#vistaPrevia').attr('src', "img/logo-Solo.png");
+
+    //El input del archivo lo vigilamos con un "delegado"
+    jQuery('#botonera').on('change', '#archivo', function(e) {
+        window.mostrarVistaPrevia();
+    });
+
+    //El botón Cancelar lo vigilamos normalmente
+  //  jQuery('#cancelar').on('click', function(e) {
+    //    var objeto = jQuery('#archivo');
+     //   objeto.replaceWith(objeto.val('').clone());
+
+       // jQuery('#vistaPrevia').attr('src', window.imagenVacia);
+      //  jQuery('#infoNombre').text('[Seleccione una imagen]');
+       // jQuery('#infoTamaño').text('');
+    //});
+
+});
+function imgPorDefecto(){
+
+	jQuery('#vistaPrevia').attr('src', "img/logo-Solo.png");
+	document.getElementById("linkProcedimiento").style.display = "block";
+    document.getElementById("procedimiento").style.display = "none";
+}
+
+	 function mostrar(dato){
+        if(dato=="1"){
+            document.getElementById("linkProcedimiento").style.display = "block";
+            document.getElementById("procedimiento").style.display = "none";
+            document.getElementById("procedimiento").value=" ";
+            
+          }else{
+          	document.getElementById("linkProcedimiento").style.display = "none";
+          	document.getElementById("linkProcedimiento").value=" ";
+            document.getElementById("procedimiento").style.display = "block";
+          }
+}
